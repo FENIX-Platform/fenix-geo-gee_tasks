391574ad0f2572d2898a92b72a276ba6df76ab81
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2016-10-28 17:59:21 +0200

    Add view for monit url

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+
+from .models import Job
+
+
+def monit(request):
+    return render(request, 'jobs/index.html')
