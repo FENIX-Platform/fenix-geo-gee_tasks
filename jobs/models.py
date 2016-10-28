e2a4f70e8aeeedb84a20a82a6735f4e7270bbe30
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2016-10-28 17:09:28 +0200

    Add job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, null=True, blank=True)
+    created = models.DateTimeField(default=timezone.now)
+    completed = models.DateTimeField(null=True, blank=True)
+    celery_id = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __unicode__(self):
+        return self.name
